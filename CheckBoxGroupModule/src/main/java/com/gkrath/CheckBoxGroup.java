53c61ce7ea9beba9c674e9749b9241766dc56938
gopal <grath91@gmail.com>
2015-12-16 18:09:50 +0530

    Update

--- CheckBoxGroupModule/src/main/java/com/gkrath/CheckBoxGroup.java
+++ CheckBoxGroupModule/src/main/java/com/gkrath/CheckBoxGroup.java
@@ -17,7 +17,7 @@ public class CheckBoxGroup extends LinearLayout {
 
     // holds the checked id; the selection is empty by default
     private int mCheckedId = -1;
-    // tracks children radio buttons checked state
+    // tracks children CheckBox checked state
     private CompoundButton.OnCheckedChangeListener mChildOnCheckedChangeListener;
     // when true, mOnCheckedChangeListener discards events
     private boolean mProtectFromCheckedChange = false;
@@ -39,7 +39,7 @@ public class CheckBoxGroup extends LinearLayout {
     public CheckBoxGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        // retrieve selected radio button as requested by the user in the
+        // retrieve selected CheckBox as requested by the user in the
         // XML layout file
         TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.CheckBoxGroup, R.attr.checkBoxStyle, 0);
 
@@ -76,7 +76,7 @@ public class CheckBoxGroup extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
 
-        // checks the appropriate radio button as requested in the XML file
+        // checks the appropriate CheckBox as requested in the XML file
         if (mCheckedId != -1) {
             mProtectFromCheckedChange = true;
             setCheckedStateForView(mCheckedId, true);
@@ -104,11 +104,11 @@ public class CheckBoxGroup extends LinearLayout {
     }
 
     /**
-     * <p>Sets the selection to the radio button whose identifier is passed in
+     * <p>Sets the selection to the CheckBox whose identifier is passed in
      * parameter. Using -1 as the selection identifier clears the selection;
      * such an operation is equivalent to invoking {@link #clearCheck()}.</p>
      *
-     * @param id the unique id of the radio button to select in this group
+     * @param id the unique id of the CheckBox to select in this group
      * @see #getCheckedCheckBoxId()
      * @see #clearCheck()
      */
@@ -146,11 +146,11 @@ public class CheckBoxGroup extends LinearLayout {
     }
 
     /**
-     * <p>Returns the identifier of the selected radio button in this group.
+     * <p>Returns the identifier of the selected CheckBox in this group.
      * Upon empty selection, the returned value is -1.</p>
      *
-     * @return the unique id of the selected radio button in this group
-     * @attr ref android.R.styleable#RadioGroup_checkedButton
+     * @return the unique id of the selected CheckBox in this group
+     * @attr ref android.R.styleable#CheckBoxGroup_checkedButton
      * @see #check(int)
      * @see #clearCheck()
      */
@@ -160,7 +160,7 @@ public class CheckBoxGroup extends LinearLayout {
     }
 
     /**
-     * <p>Clears the selection. When the selection is cleared, no radio button
+     * <p>Clears the selection. When the selection is cleared, no CheckBox
      * in this group is selected and {@link #getCheckedCheckBoxId()} returns
      * null.</p>
      *
@@ -172,7 +172,7 @@ public class CheckBoxGroup extends LinearLayout {
     }
 
     /**
-     * <p>Register a callback to be invoked when the checked radio button
+     * <p>Register a callback to be invoked when the checked  CheckBox
      * changes in this group.</p>
      *
      * @param listener the callback to call on checked state change
@@ -282,15 +282,15 @@ public class CheckBoxGroup extends LinearLayout {
 
     /**
      * <p>Interface definition for a callback to be invoked when the checked
-     * radio button changed in this group.</p>
+     * CheckBox changed in this group.</p>
      */
     public interface OnCheckedChangeListener {
         /**
-         * <p>Called when the checked radio button has changed. When the
+         * <p>Called when the checked CheckBox has changed. When the
          * selection is cleared, checkedId is -1.</p>
          *
-         * @param group     the group in which the checked radio button has changed
-         * @param checkedId the unique identifier of the newly checked radio button
+         * @param group     the group in which the checked CheckBox has changed
+         * @param checkedId the unique identifier of the newly checked CheckBox
          */
         public void onCheckedChanged(CheckBoxGroup group, @IdRes int checkedId);
     }
